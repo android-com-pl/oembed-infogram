1cfcdaec22614fa2a536c520045d03894a565dd4
Rafał Całka <25438601+rafaucau@users.noreply.github.com>
2025-04-08 10:54:10 +0200

    refactor: remove unnecessary complexity

--- oembed-infogram.php
+++ oembed-infogram.php
@@ -3,7 +3,7 @@
  * @wordpress-plugin
  * Plugin Name:       oEmbed Infogram
  * Description:       A simple plugin that adds support for embedding Infogram.
- * Version:           1.1.3
+ * Version:           1.2.0
  * Plugin URI:        https://github.com/android-com-pl/oembed-infogram
  * Author:            android.com.pl
  * Author URI:        https://android.com.pl/
@@ -13,63 +13,31 @@
 
 namespace ACP\oEmbed;
 
-defined( 'ABSPATH' ) || die;
+if ( ! defined( 'ABSPATH' ) ) {
+	http_response_code( 403 );
+	exit();
+}
 
 class Infogram {
-	/**
-	 * The unique instance of the plugin
-	 * @var ?Infogram
-	 */
-	protected static ?Infogram $instance = null;
-
-	/**
-	 * Gets an instance of plugin
-	 * @return Infogram
-	 */
-	public static function get_instance(): Infogram {
-		if ( null === self::$instance ) {
-			self::$instance = new self();
-		}
-
-		return self::$instance;
+	public static function init() {
+		add_action( 'init', [ self::class, 'add_provider' ] );
+		add_filter( 'amp_content_embed_handlers', [ self::class, 'add_amp_handler' ], 10, 2 );
+		add_filter( 'plugin_row_meta', [ self::class, 'add_plugin_row_meta_links' ], 10, 4 );
 	}
 
-	private function __construct() {
-		add_action( 'init', [ $this, 'add_provider' ] );
-		add_filter( 'amp_content_embed_handlers', [ $this, 'add_amp_handler' ], 10, 2 );
-		add_filter( 'plugin_row_meta', [ $this, 'add_plugin_row_meta_links' ], 10, 4 );
-	}
-
-	public function add_provider(): void {
+	public static function add_provider(): void {
 		wp_oembed_add_provider( 'https://infogram.com/*', 'https://infogram.com/oembed/?format=json' );
 	}
 
-	/**
-	 * AMP Support
-	 *
-	 * @param array $handler_classes
-	 *
-	 * @return array
-	 * @see https://amp-wp.org/documentation/playbooks/custom-embed-handler/
-	 */
-	public function add_amp_handler( array $handler_classes ): array {
+	/** @see https://amp-wp.org/documentation/playbooks/custom-embed-handler/ */
+	public static function add_amp_handler( array $handler_classes ): array {
 		require_once( plugin_dir_path( __FILE__ ) . 'class-amp-infogram-oembed-handler.php' );
 		$handler_classes[ __NAMESPACE__ . '\\Infogram_Embed_Handler' ] = [];
 
 		return $handler_classes;
 	}
 
-	/**
-	 * Adds elements to the plugin's meta row
-	 *
-	 * @param string[] $plugin_meta
-	 * @param string $plugin_file
-	 * @param array $plugin_data
-	 * @param string $status
-	 *
-	 * @return array
-	 */
-	public function add_plugin_row_meta_links( array $plugin_meta, string $plugin_file, array $plugin_data, string $status ): array {
+	public static function add_plugin_row_meta_links( array $plugin_meta, string $plugin_file ): array {
 		if ( str_contains( $plugin_file, basename( __FILE__ ) ) ) {
 			$plugin_meta[] = '<a href="https://github.com/android-com-pl/oembed-infogram">GitHub</a>';
 			$plugin_meta[] = sprintf(
@@ -82,4 +50,4 @@ class Infogram {
 	}
 }
 
-$oembed_infogram = Infogram::get_instance();
+Infogram::init();
